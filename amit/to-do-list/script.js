4b4e2ffe7482383b8698e5f135925cf70a7c1d0b
AmitGaire1 <amit.gaire03@gmail.com>
2024-03-18 11:49:42 +0545

    to-do-list

---
+++ amit/to-do-list/script.js
@@ -0,0 +1,33 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask(){
+    if (inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";
+}
+
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        
+    }
+
+}, false);
+
+
