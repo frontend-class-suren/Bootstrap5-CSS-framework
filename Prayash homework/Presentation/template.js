2754f4704df4cad2570668c384ed594e7050b2e7
unknown <raanaa689@gmail.com>
2024-04-01 11:51:18 +0545

    done

---
+++ Prayash homework/Presentation/template.js
@@ -0,0 +1,52 @@
+
+function validateForm(){
+    let name=document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+    let nameError = document.getElementById("nameError");
+    let emailError = document.getElementById("emailError");
+    let messageError = document.getElementById("messageError");
+    let isValid = true;
+
+    nameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent ="";
+
+    if(name==""){
+        nameError.textContent = "Name is required.";
+        isValid = false;
+    }
+    if(email ==""){
+        emailError.textContent = "Email is required.";
+        isValid = false;
+    }else if(!isValidEmail(email)){
+        emailError.textContent = "Invalid email format.";
+        isValid = false;
+    }
+    if(message ==""){
+        messageError.textContent = "Message is required.";
+        isValid = false;
+    }
+    return isValid;
+}
+function isValidEmail(email) {
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let showMoreButton = document.getElementById("showMoreButton");
+    let hiddenItems = document.querySelectorAll("#workItems .col-md-4[style='display: none;']");
+
+    // Event listener for the "SHOW ME MORE" button
+    showMoreButton.addEventListener("click", function() {
+        // Toggle display of hidden items
+        hiddenItems.forEach(function(item) {
+            item.style.display = "block";
+        });
+        // Hide the button after revealing all items
+        showMoreButton.style.display = "none";
+    });
+});
+
